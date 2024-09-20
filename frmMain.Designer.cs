6b255d79324889a1b43694b7ec631c72639143ca
Pablo Gener <pablogener@hotmail.com>
2024-09-20 07:49:40 -0300

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace BackgroundDominator_v2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCarpeta = new System.Windows.Forms.TextBox();
+            this.cmdCarpeta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCarpeta
+            // 
+            this.txtCarpeta.AllowDrop = true;
+            this.txtCarpeta.Location = new System.Drawing.Point(12, 12);
+            this.txtCarpeta.Name = "txtCarpeta";
+            this.txtCarpeta.Size = new System.Drawing.Size(731, 20);
+            this.txtCarpeta.TabIndex = 0;
+            this.txtCarpeta.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtCarpeta_DragDrop);
+            this.txtCarpeta.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtCarpeta_DragEnter);
+            // 
+            // cmdCarpeta
+            // 
+            this.cmdCarpeta.Location = new System.Drawing.Point(749, 12);
+            this.cmdCarpeta.Name = "cmdCarpeta";
+            this.cmdCarpeta.Size = new System.Drawing.Size(39, 23);
+            this.cmdCarpeta.TabIndex = 1;
+            this.cmdCarpeta.Text = "...";
+            this.cmdCarpeta.UseVisualStyleBackColor = true;
+            this.cmdCarpeta.Click += new System.EventHandler(this.cmdCarpeta_Click);
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmdCarpeta);
+            this.Controls.Add(this.txtCarpeta);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Background Dominator v2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCarpeta;
+        private System.Windows.Forms.Button cmdCarpeta;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BackgroundDominator_v2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdCarpeta_Click(object sender, EventArgs e)
+        {
+            List<string> strImagenes = new List<string>();
+            DirectoryInfo dinfo = new DirectoryInfo(txtCarpeta.Text);
+            FileInfo[] Imagenes = dinfo.GetFiles(@"*.jpg");
+            foreach (FileInfo imagen in Imagenes)
+            {
+                //lstImagenes.Items.Add(imagen.Name);
+                strImagenes.Add(imagen.Name);
+            }
+
+            foreach (string imagen in strImagenes) 
+            {
+                Debug.WriteLine("Imagen: " + imagen);
+            }
+        }
+
+        private void txtCarpeta_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] archivos = (string[])e.Data.GetData(DataFormats.FileDrop);
+                Debug.WriteLine("Carpeta: " + archivos[0]);
+
+                DirectoryInfo carpeta = new DirectoryInfo(Path.GetFullPath(archivos[0]));
+                if (carpeta.Exists) 
+                {
+                    txtCarpeta.Text = carpeta.ToString();
+                }
+
+            }
+        }
+
+        private void txtCarpeta_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+    }
+}
